e8b2b28866f822236a145c37790306ccc4245c60
Ayush-Bhandari-2710 <ayush2710social@gmail.com>
2026-02-14 13:58:36 +0530

    Valentine update2

--- src/components/valentine/HeartSection.tsx
+++ src/components/valentine/HeartSection.tsx
@@ -4,7 +4,7 @@ import { motion, useInView } from 'framer-motion';
 import img0 from '../../assets/image.png';
 import img1 from '../../assets/image1.png';
 import img2 from '../../assets/image2.png';
-import img3 from '../../assets/image3.png';
+import img3 from '../../assets/image3.jpeg';
 import img4 from '../../assets/image4.png';
 import img5 from '../../assets/image5.png';
 import img6 from '../../assets/image6.png';
